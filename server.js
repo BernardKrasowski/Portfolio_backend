40ed6f6d2f3e1afb69983472810c035fe0b025db
Bernard <bernardkrasowski111@gmail.com>
2021-07-28 18:31:14 +0100

    bcrypt password and compare with saved password on server

--- server.js
+++ server.js
@@ -11,27 +11,49 @@ app.use(express.json());
 app.use(cors());
 
 //Routes  
-const user = [];
+const admin = [
+  {
+    name: "benek",
+    password: "$2b$10$h9zfKLD3fsbHjhZXl/.UN.Zm7b9wNEe1/yYnuvBicarHPSG9xOQ2W"
+  }
+]
 
 app.get('/login', (req, res) => {
-  res.status(200).send(user)
+  res.status(200).send(admin)
 })
 
 
-app.post('/login', async (req, res) => {
+// app.post('/login', async (req, res) => {
 
-  try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
-    console.log(salt)
-    console.log(hashedPassword)
-    const admin = { name: req.body.name, password: hashedPassword };
-    user.push(admin)
-    res.status(201).send(true)
-  } catch {
-    res.status(500).send()
+//   try {
+//     const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//     const user = { name: req.body.name, password: hashedPassword };
+//     admin.push(user)
+//     res.status(201).send(true)
+//   } catch {
+//     res.status(500).send()
+//   }
+// })
+app.post('/login/admin', async (req, res) => {
+
+  const user = admin.find(item => item.name === req.body.name);
+
+
+  if (user === null) {
+    return res.status(400).send('Cannot find user')
   }
+  // console.log(await bcrypt.compare(req.body.password, user.password))
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
 
+      res.json('Succes')
+
+    } else {
+      res.json('Not allowed')
+    }
+  } catch {
+    res.status(500).json('Wrong')
+  }
 })
 
 
